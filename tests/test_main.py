daa92a491527cc1823e7acee35ad4eebc21e56ea
Danny Sepler <dannysepler@gmail.com>
2021-12-28 12:29:44 -0500

    Only rewrite camelCase when it's a test file (#21)

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,3 +20,32 @@ def test_preserves_blank_line(f):
 
     assert f.read_text() == 'assert a == b\n'
     assert ret == 1
+
+
+def test_rewrite_method_names_in_test_file(f):
+    f.write_text('''
+class TestThing(unittest.TestCase):
+    def testCamelCase(self):
+        self.assertEqual(1, 2)
+''')
+    ret = main([str(f)])
+
+    assert f.read_text() == '''
+class TestThing:
+    def test_camel_case(self):
+        assert 1 == 2
+'''
+    assert ret == 1
+
+
+def test_doesnt_rewrite_method_names_in_non_test_file(f):
+    orig_contents = '''
+def camelCaseButNotTest():
+    print("don't rewrite me")
+'''
+    f.write_text(orig_contents)
+
+    ret = main([str(f)])
+
+    assert f.read_text() == orig_contents
+    assert ret == 0
